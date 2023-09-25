007d1ac97098a294a94b0375dc5f9233698a4e43
basokant <basokanthan@gmail.com>
2023-09-24 22:34:12 -0400

    create group create form component

---
+++ src/lib/components/GroupCreate.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import gameQueueStore from "$lib/stores/game";
+    import type { Nullable, Player } from "$lib/types";
+    import { z } from "zod";
+    import PlayerSelect from "./PlayerSelect.svelte";
+    import Button from "./ui/button/button.svelte";
+
+    const players: Nullable<Player>[] = Array(4).fill(null)
+    const playerSchema = z.object({
+        id: z.number(),
+        name: z.string()
+    })
+    const playersSchema = z.array(playerSchema).length(4);
+
+    let isValid = false;
+    
+    $: {
+        try {
+            !!playersSchema.parse(players);
+            isValid = true;
+        } catch (error) {
+            isValid = false;
+        }
+    };
+</script>
+
+<form class="flex gap-4" on:submit|preventDefault={() => {
+    const parsedPlayers = playersSchema.parse(players);
+    gameQueueStore.enqueueGroup(parsedPlayers);
+    players.map(() => null);
+    const emptyCourt = $gameQueueStore.courts.findIndex((game) => game === null);
+    if (emptyCourt !== -1) {
+        gameQueueStore.addNewGame(emptyCourt);
+    }
+}}>
+    {#each Array(4) as _, i}
+        <PlayerSelect inputName={`player${i}`} onPlayerChange={(player) => { players[i] = player }}/>
+    {/each}
+    <Button type="submit" disabled={!isValid}>Add Group</Button>
+</form>
